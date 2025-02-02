07184818359fb46a06592e68485922db65eff2f5
Norman <norman.gaza@gmail.com>
2025-02-03 00:14:46 +0700

    Update accordion

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for(i = 0; i < acc.length; i++){
-    acc[i].addEventListener("click", function(){
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight == null;
-        } else{
-            panel.style.maxHeight = panel.scrollHeight + "px"
-        }
-        
-    });
+function toggleAccordion(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
 }
 
-function OpenOutlook(){
-    window.location.href ="mailto:mohammad.norman@ui.ac.id";
+function showContactInfo() {
+    var contactInfo = document.getElementById("contact-info");
+    if (contactInfo.style.display === "none") {
+        contactInfo.style.display = "block";
+    } else {
+        contactInfo.style.display = "none";
+    }
 }
 
-function OpenLinkedIn(){
-    window.location.href="https://www.linkedin.com/in/mohammad-norman-gaza-laksono-5979a11a7/";
-}
-
-function OpenGithub(){
-    window.location.href="https://github.com/Normanthen";
-}
